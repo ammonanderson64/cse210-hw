fe2a34b423a851eeb61d7f7f1abfa3b0522979c4
ammonanderson64 <and21110@byui.edu>
2024-11-09 21:28:47 -0700

    Update 03refrence.cs

--- prove/Develop03/03refrence.cs
+++ prove/Develop03/03refrence.cs
@@ -1,1 +1,17 @@
+namespace ScriptureApp
+{
+    public class Reference
+    {
+        public string Text { get; private set; }
 
+        public Reference(string text)
+        {
+            Text = text;
+        }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
